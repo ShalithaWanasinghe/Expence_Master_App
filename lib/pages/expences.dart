e50483804c67592b536057debb6271b812aa009a
ShalithaWanasinghe <shalithawanasinghe@gmail.com>
2024-04-28 22:38:16 +0530

    App bar design

---
+++ lib/pages/expences.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Expences extends StatefulWidget {
+  const Expences({super.key});
+
+  @override
+  State<Expences> createState() => _ExpencesState();
+}
+
+class _ExpencesState extends State<Expences> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Expence Master",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromARGB(255, 77, 4, 195),
+        elevation: 5,
+        actions: [
+          Container(
+            color: Colors.yellow,
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.add,
+                color: Colors.black,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
